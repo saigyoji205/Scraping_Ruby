55787c6599a4680c96fc42d37ef3ddaebf74188c
Hirokazu <saigyoji205@gmail.com>
2015-05-26 13:22:44 +0900

    update seminar.rb

--- crawl_seminar.rb
+++ crawl_seminar.rb
@@ -4,28 +4,35 @@ require 'open-uri'
 require 'nokogiri'
 require 'sqlite3'
 require 'json'
+require 'robotex'
 urls =[]
 
-db = SQLite3::Database.new('crawling.db')
-db.execute('CREATE TABLE IF NOT EXISTS seminars (title varchar(100), image_url varchar(200))')
+db = SQLite3::Database.new('crawling_detail.db')
+db.execute('CREATE TABLE IF NOT EXISTS seminars_presenter (title varchar(100), image_url varchar(200),presenter varchar(200),presenter_detail varchar(300))')
 
-### 1650~1699の範囲でページを配列urlsに格納
-(1650 ... 1699).each do |parameta|
-	urls.push("https://www.goodfind.jp/2016/seminar/" + parameta.to_s)
+user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36'
+robotex = Robotex.new
+
+### 1649~1699の範囲でページを配列urlsに格納
+(1649 ... 1699).each do |parameta|
+		urls.push("https://www.goodfind.jp/2016/seminar/" + parameta.to_s
+)
 end
 
-
+### urlsからデータベースにtitleとimg_urlを格納
 urls.each_with_index do |url,index|
 	charset = nil
 	begin
 		### 格納したページをオープン
-		html = open(url) do |f|
+		html = open(url,"User-Agent=" => user_agent) do |f|
 			charset = f.charset
 			f.read
 		end
 		
 		### HTMLページをタグ構造ごとに分解
 		doc = Nokogiri::HTML.parse(html,nil,charset)
+		
+		#セミナータイトル
 		puts doc.css('title').text
 		
 		### <img src = data> 
@@ -33,7 +40,22 @@ urls.each_with_index do |url,index|
 		full_image_url = url + img_url
 		puts full_image_url
 		
-		db.execute 'INSERT INTO seminars values ( ?, ? );', ["#{doc.css('title').text}", "#{full_image_url}"]
+		### 登壇者&肩書き
+		presenter = doc.css('#section_1 > div > p > span').text
+		puts presenter
+		
+		### 登壇者詳細
+		presenter_detail = doc.css('#section_2 > div > p > span').text
+		puts presenter_detail
+		
+		p robotex.allowed?(url)
+		
+		
+		db.transaction do
+			sql = "insert into seminars_presenter values (?,?,?,?)"
+			db.execute(sql,"#{doc.css('title').text}","#{full_image_url}","#{presenter}","#{presenter_detail}")
+		end
+		
 		rescue
 			puts "error"
 	end
